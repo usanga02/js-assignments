924d39d9ed1b09bcbcc5215440e713a72cf96e17
Usanga Nsikak <31377369+usanga02@users.noreply.github.com>
2022-11-16 12:49:39 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+/*
+1.   Write a function to that will take to parameters and print out the sum on the screen using alert()
+2.   Define a Person class that can be used to create person objects such that:
+    (i) It will take in the person's firstname and surname and age
+    (ii) It will print the person's fullname when a function called person.fullName is called
+    (iii) It will print out "Adult" if the person is 18 or older and print Child if the person is below 18
+3.  Write any example of a nested array (3 Dimensional Array) and a nested object
+4.  Create a branch off this repo and push your updates to it
+*/
